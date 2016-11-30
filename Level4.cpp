c9e80a189d597099102d4904d0067378885792f5
Saya2020 <bobcatato@gmail.com>
2016-11-30 09:10:25 +0000

    Update Level4.cpp

--- Level4.cpp
+++ Level4.cpp
@@ -1,1 +1,37 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+void insertNameHere() {
+  int thisWontBeFun[1000][1000][1000];
+  return(thisWontBeFun);
+}
+
+int main(int argc, const char *argv[]) {
+  
+  /*
+  #ifdef _WIN32
+    int Windows = 1;
+  #endif
+  #ifdef __APPLE__
+    int Apple = 1;
+  #endif
+  
+  if (Windows == 1) {
+    
+  }
+  */
+  
+  /*
+  *
+  * Simple but effective
+  *
+  * The below takes up a good chunck of memory
+  */
+  
+  lool = insertNameHere();
+  
+  return 0;
+}
